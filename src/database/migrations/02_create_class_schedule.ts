8d388331e0688d30bc044efe49492eeb5df84d44
Lucas Borges <lscborges1@gmail.com>
2020-10-07 19:40:23 -0300

    Added connections controller and route

--- src/database/migrations/02_create_class_schedule.ts
+++ src/database/migrations/02_create_class_schedule.ts
@@ -1,6 +1,6 @@
 import Knex from 'knex';
 
-export async function up(knex:Knex) {
+export async function up(knex: Knex) {
   return knex.schema.createTable('class_schedule', table => {
     table.increments('id').primary();
 
@@ -8,17 +8,16 @@ export async function up(knex:Knex) {
     table.integer('from').notNullable();
     table.integer('to').notNullable();
 
-    table.integer('class_id')
-    .notNullable()
-    .references('id')
-    .inTable('classes')
-    .onUpdate('CASCADE')
-    .onDelete('CASCADE')
-
-
+    table
+      .integer('class_id')
+      .notNullable()
+      .references('id')
+      .inTable('classes')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
   });
 }
 
-export async function down(knex:Knex) {
+export async function down(knex: Knex) {
   return knex.schema.dropTable('classe_schedule');
 }
